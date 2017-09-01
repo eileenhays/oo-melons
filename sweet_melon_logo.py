a1a6a45115f5d1ac4a7a510e6363c53295e73c52
Hackbright Student <no-reply@hackbrightacademy.com>
2017-09-01 13:03:02 -0700

    started on ubermelon logo

--- sweet_melon_logo.py
+++ sweet_melon_logo.py
@@ -1,2 +1,17 @@
-import cs1graphics
+import cs1graphics as cg
 
+paper = cg.Canvas()
+paper.setBackgroundColor('black')
+paper.setWidth(600)
+paper.setHeight(600)
+paper.setTitle('Melon Logo')
+
+uber_melon = cg.Ellipse(400, 750, cg.Point(300, 200))
+
+uber_melon.setBorderWidth(100)
+
+uber_melon.setBorderColor("green")
+
+uber_melon.setFillColor('red')
+
+paper.add(uber_melon)
